ce2510c3aacd2835a7dff5baa24465de84222966
Robert Schneider <robert-schneider@live.de>
2023-12-04 11:36:17 +0100

    Day04 Optimization

--- Day04/Program.cs
+++ Day04/Program.cs
@@ -1,35 +1,30 @@
-﻿using Shared;
+﻿using System.Diagnostics;
+using Shared;
 
 namespace Day04 {
     internal class Program {
         static void Main(string[] args) {
-            if (!ArgsValidator.IsValidArgs(args))
-                return;
+            if (!ArgsValidator.IsValidArgs(args)) return;
 
             double p1_score = 0;
             int p2_score = 0;
 
             string[] lines = File.ReadAllLines(args[0]);
-            List<int> copies = lines.Select(x => 1).ToList();
+            int[] copies = lines.Select(x => 1).ToArray();
 
             for (int i = 0; i < lines.Length; i++) {
                 string[] parts = lines[i].Split(':', '|');
                 string[] winningNumbers = parts[1].Split(' ', StringSplitOptions.RemoveEmptyEntries);
                 string[] scratchNumbers = parts[2].Split(' ', StringSplitOptions.RemoveEmptyEntries);
-                int winningCounter = 0;
-                foreach (string scratchNumber in scratchNumbers) {
-                    if (winningNumbers.Contains(scratchNumber)) {
-                        winningCounter++;
-                    }
-                }
+                int winningCounter = scratchNumbers.Where(x => winningNumbers.Contains(x)).Count();
+
                 if (winningCounter > 0) {
                     p1_score += Math.Pow(2, winningCounter - 1);
                 }
+
                 p2_score += copies[i];
-                for (int win = 1; win <= winningCounter; win++) {
-                    if (i + win < copies.Count) {
-                        copies[i + win] += copies[i];
-                    }
+                for (int win = 1; win <= Math.Min(winningCounter, copies.Length) ; win++) {
+                    copies[i + win] += copies[i];
                 }
             }
 
