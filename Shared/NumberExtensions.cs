9926c3c571537347a470142f00c8e4033c78d3a1
Robert Schneider <robert-schneider@live.de>
2023-12-05 11:10:35 +0100

    Day05

---
+++ Shared/NumberExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace Shared {
+    public static class NumberExtensions {
+        public static bool IsBetween(this long number, long min, long max) {
+            return number >= min && number <= max;
+        }
+    }
+}
