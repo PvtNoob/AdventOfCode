81f16e6fa6aca6393d7e3596f37c612e1c414e93
Robert Schneider <robert-schneider@live.de>
2023-12-06 09:50:25 +0100

    Day06 optimized

--- Day06/Program.cs
+++ Day06/Program.cs
@@ -1,44 +1,44 @@
-﻿using Shared;
+﻿using System.Diagnostics;
+using Shared;
 
 namespace Day06 {
     internal class Program {
         static void Main(string[] args) {
             if (!ArgsValidator.IsValidArgs(args)) return;
 
-            int p1_score = 0;
-            int p2_score = 0;
-
             string[] lines = File.ReadAllLines(args[0]);
-            int[] times = GetValues(lines[0]);
-            int[] distances = GetValues(lines[1]);
-            int[] amountOfWinOptions = times.Select(x => 0).ToArray();
 
-            for (int race = 0; race < times.Length; race++) {
-                for (int pushTime = 1; pushTime < times[race] - 1; pushTime++) {
-                    int distance = pushTime * (times[race] - pushTime);
-                    if (distance > distances[race]) {
-                        amountOfWinOptions[race]++;
+            List<long> times = GetValues(lines[0]);
+            List<long> distances = GetValues(lines[1]);
+            long[] amountOfWinOptionsList = times.Select(x => 0L).ToArray();
+
+            for (int race = 0; race < times.Count; race++) {
+                long time = times[race];
+                long distance = distances[race];
+                long amountOfWinOptions = 0;
+
+                for (int pushTime = 1; pushTime < time; pushTime++) {
+                    long runDistance = pushTime * (time - pushTime);
+                    if (runDistance > distance) {
+                        amountOfWinOptions++;
+                    } else if(amountOfWinOptions > 0) {
+                        amountOfWinOptionsList[race] = amountOfWinOptions;
+                        break;
                     }
                 }
             }
 
-            p1_score = amountOfWinOptions.Aggregate(1, (a, b) => a * b);
-
-            long p2Time = long.Parse(lines[0].Split(':')[1].Replace(" ", ""));
-            long p2Distance = long.Parse(lines[1].Split(':')[1].Replace(" ", ""));
-
-            for (long pushTime = 1; pushTime < p2Time - 1; pushTime++) {
-                long distance = pushTime * (p2Time - pushTime);
-                if (distance > p2Distance) {
-                    p2_score++;
-                }
-            }
+            long p1_score = amountOfWinOptionsList.SkipLast(1).Aggregate(1, (long a, long b) => a * b);
+            long p2_score = amountOfWinOptionsList.Last();
 
             Console.WriteLine($"Part1 Result: {p1_score}\nPart2 Result: {p2_score}");
         }
 
-        private static int[] GetValues(string line) {
-            return line.Split(':')[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+        private static List<long> GetValues(string line) {
+            string numbers = line.Split(':')[1];
+            List<long> values = numbers.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToList();
+            values.Add(long.Parse(numbers.Replace(" ", "")));
+            return values;
         }
     }
 }
