52b869e681a980781faa890d8e1e5834db031052
Robert Schneider <robert-schneider@live.de>
2023-12-07 17:25:53 +0100

    Day07

---
+++ Day07/Hand.cs
@@ -0,0 +1,63 @@
+﻿namespace Day07 {
+    internal class Hand : IComparable {
+        private readonly (char card, int value)[] CardValues;
+        private readonly int[] Cards;
+        private readonly HandType Type;
+
+        public long Bid { get; private set; }
+
+        public Hand((char card, int value)[] cardValues, string cards, int bid) {
+            CardValues = cardValues;
+            Cards = cards.Select(ch => CardValues.First(cardValue => cardValue.card == ch).value).ToArray();
+            Bid = bid;
+            Type = GetHandType();
+        }
+
+        public HandType GetHandType() {
+            List<int> amounts = [];
+            for (int cardValue = 0; cardValue <= 12; cardValue++) {
+                amounts.Add(Cards.Count(handcard => handcard == cardValue));
+            }
+            amounts = amounts.OrderByDescending(x => x).ToList();
+            int biggestAmount = amounts[0];
+            int secondBiggestAmount = amounts[1];
+            int amountOfJoker = Cards.Count(handcard => handcard == -1);
+
+            if (biggestAmount + amountOfJoker == 5) {
+                return HandType.FiveOfAKind;
+            } else if (biggestAmount + amountOfJoker == 4) {
+                return HandType.FourOfAKind;
+            } else if (biggestAmount + amountOfJoker == 3) {
+                if (secondBiggestAmount == 2) {
+                    return HandType.FullHouse;
+                } else {
+                    return HandType.ThreeOfAKind;
+                }
+            } else if (biggestAmount + secondBiggestAmount + amountOfJoker == 4) {
+                return HandType.TwoPair;
+            } else if (biggestAmount + amountOfJoker == 2) {
+                return HandType.OnePair;
+            } else {
+                return HandType.HighCard;
+            }
+        }
+
+        public int CompareTo(object? obj) {
+            Hand b = (Hand)obj;
+            if (Type > b.Type) {
+                return 1;
+            } else if (Type < b.Type) {
+                return -1;
+            } else {
+                for (int i = 0; i < Cards.Length; i++) {
+                    if (Cards[i] > b.Cards[i]) {
+                        return 1;
+                    } else if (Cards[i] < b.Cards[i]) {
+                        return -1;
+                    }
+                }
+                return 0;
+            }
+        }
+    }
+}
---
+++ Day07/Program.cs
@@ -0,0 +1,34 @@
+﻿using Shared;
+
+namespace Day07 {
+    internal class Program {
+        private static readonly (char card, int value)[] p1CardValues = [('A', 12), ('K', 11), ('Q', 10), ('J', 9), ('T', 8), ('9', 7), ('8', 6), ('7', 5), ('6', 4), ('5', 3), ('4', 2), ('3', 1), ('2', 0),];
+        private static readonly (char card, int value)[] p2CardValues = [('A', 12), ('K', 11), ('Q', 10), ('J', -1), ('T', 8), ('9', 7), ('8', 6), ('7', 5), ('6', 4), ('5', 3), ('4', 2), ('3', 1), ('2', 0),];
+
+        static void Main(string[] args) {
+            if (!ArgsValidator.IsValidArgs(args)) return;
+
+            string[] lines = File.ReadAllLines(args[0]);
+            Hand[] p1Hands = lines.Select(line => ParseLine(line, p1CardValues)).ToArray();
+            Hand[] p2Hands = lines.Select(line => ParseLine(line, p2CardValues)).ToArray();
+
+            Array.Sort(p1Hands);
+            Array.Sort(p2Hands);
+
+            long p1_score = 0;
+            long p2_score = 0;
+
+            for (int i = 1; i <= p1Hands.Length; i++) {
+                p1_score += p1Hands[i - 1].Bid * i;
+                p2_score += p2Hands[i - 1].Bid * i;
+            }
+
+            Console.WriteLine($"Part1 Result: {p1_score}\nPart2 Result: {p2_score}");
+        }
+
+        private static Hand ParseLine(string line, (char card, int value)[] cardValues) {
+            string[] parts = line.Split(" ");
+            return new(cardValues, parts[0], int.Parse(parts[1]));
+        }
+    }
+}
