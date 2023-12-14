83a8e20a82c10eb21ccaaae42c93f4008168385d
Robert Schneider <robert-schneider@live.de>
2023-12-14 12:34:15 +0100

    Day12 little improvements

--- Day12/Program.cs
+++ Day12/Program.cs
@@ -5,6 +5,7 @@ namespace Day12 {
     internal class Program {
         private static List<int> _hashTagIndexes = [];
         private static List<int> _possiblePlaceIndexes = [];
+        private static int _amountOfIndexesToPlace = 0;
 
         static void Main(string[] args) {
             if (!ArgsValidator.IsValidArgs(args)) return;
@@ -14,11 +15,12 @@ namespace Day12 {
             long p1_score = 0;
             long p2_score = 0;
 
-            foreach(string line in File.ReadLines(args[0])) {
+            foreach (string line in File.ReadLines(args[0])) {
                 string field = line.Split(' ')[0];
                 BuildFieldCaches(field);
                 int[] lenghts = line.Split(' ')[1].Split(',').Select(int.Parse).ToArray();
-                p1_score += TryCombinations(field, lenghts, []);
+                _amountOfIndexesToPlace = lenghts.Sum();
+                p1_score += TryCombinations(field, lenghts, [], 0);
 
                 fieldWatch.Start();
                 string unfoldField = $"{field}?{field}?{field}?{field}?{field}";
@@ -29,7 +31,8 @@ namespace Day12 {
                 lenghts.CopyTo(unfoldLengths, lenghts.Length * 2);
                 lenghts.CopyTo(unfoldLengths, lenghts.Length * 3);
                 lenghts.CopyTo(unfoldLengths, lenghts.Length * 4);
-                long possibilities = TryCombinations(unfoldField, unfoldLengths, []);
+                _amountOfIndexesToPlace = unfoldLengths.Sum();
+                long possibilities = TryCombinations(unfoldField, unfoldLengths, [], 0);
                 Console.WriteLine($"Found after {fieldWatch.Elapsed} {possibilities} \tpossible solutions in field {field}.");
                 fieldWatch.Reset();
                 p2_score += possibilities;
@@ -39,80 +42,69 @@ namespace Day12 {
             Console.WriteLine($"Elapsed: {stopwatch.Elapsed}");
         }
 
-        private static long TryCombinations(string field, int[] lenghts, int[] lengthIndexes) {
-            if (lenghts.Length == lengthIndexes.Length) {
-                return CheckCombination(field, lenghts, lengthIndexes, partial: false) ? 1 : 0;
-            } else if(!CheckCombination(field, lenghts, lengthIndexes, partial: true)) {
+        private static long TryCombinations(string field, int[] lenghts, int[] indexList, int indexToSearch) {
+            if (_amountOfIndexesToPlace == indexList.Length) {
+                return CheckHashTags(field, lenghts, indexList, partial: false) ? 1 : 0;
+            } else if (!CheckHashTags(field, lenghts, indexList, partial: true)) {
                 return 0;
             }
 
             long possibilities = 0;
-            int indexToSearch = lengthIndexes.Length;
+            int startIndex = indexToSearch == 0 ? _possiblePlaceIndexes.First() : indexList.Last() + 2;
 
-            int startIndex;
-            if(indexToSearch == 0) {
-                //Very first index. Use first possible place, that has enough space
-                startIndex = _possiblePlaceIndexes.First();
-            } else {
-                startIndex = lengthIndexes.Last() + lenghts[indexToSearch - 1] + 1;
-            }
+            for (int index = startIndex; index <= field.Length - lenghts[indexToSearch]; index++) {
 
-            for(int i = startIndex; i <= field.Length - lenghts[indexToSearch]; i++) {
-                if (!_possiblePlaceIndexes.Contains(i)) {
+                bool impossible = false;
+                for (int checkIndex = index; checkIndex < index + lenghts[indexToSearch]; checkIndex++) {
+                    if (!_possiblePlaceIndexes.Contains(checkIndex)) {
+                        impossible = true;
+                        break;
+                    }
+                }
+                if (impossible) {
                     continue;
                 }
 
                 //Build lengthIndexes
-                int[] nextLengthIndexes = new int[indexToSearch + 1];
-                lengthIndexes.CopyTo(nextLengthIndexes, 0);
-                nextLengthIndexes[indexToSearch] = i;
+                int[] nextLengthIndexes = new int[indexList.Length + lenghts[indexToSearch]];
+                indexList.CopyTo(nextLengthIndexes, 0);
+                for (int length = 0; length < lenghts[indexToSearch]; length++) {
+                    nextLengthIndexes[indexList.Length + length] = index + length;
+                }
 
-                possibilities += TryCombinations(field, lenghts, nextLengthIndexes);
+                possibilities += TryCombinations(field, lenghts, nextLengthIndexes, indexToSearch + 1);
             }
 
             return possibilities;
         }
 
-        private static bool CheckCombination(string field, int[] lenghts, int[] lengthIndexes, bool partial) {
-            if (lengthIndexes.Length == 0) return true;
-            List<int> indexList = [];
-            for (int i = 0; i < lengthIndexes.Length; i++) {
-                for (int j = 0; j < lenghts[i]; j++) {
-                    indexList.Add(lengthIndexes[i] + j);
-                }
-            }
+        private static bool CheckHashTags(string field, int[] lenghts, int[] indexList, bool partial) {
+            if (indexList.Length == 0) return true;
 
-            //Check if all lengths are possible
-            foreach(int index in indexList) {
-                if (!_possiblePlaceIndexes.Contains(index)) {
-                    return false;
-                }
-            }
-
-            if(partial) {
+            if (partial) {
                 //Check if a # is in an empty space
-                foreach(int hashTagIndex in _hashTagIndexes) {
-                    if(hashTagIndex < lengthIndexes.Last() && !indexList.Contains(hashTagIndex)) {
+                foreach (int hashTagIndex in _hashTagIndexes) {
+                    if (hashTagIndex < indexList.Last() && !indexList.Contains(hashTagIndex)) {
                         return false;
                     }
                 }
             } else {
                 //Check if all # are placed
-                foreach(int index in _hashTagIndexes) {
-                    if(!indexList.Contains(index)) {
+                foreach (int index in _hashTagIndexes) {
+                    if (!indexList.Contains(index)) {
                         return false;
                     }
                 }
             }
-            
+
             return true;
         }
 
         private static void BuildFieldCaches(string field) {
             _hashTagIndexes.Clear();
             _possiblePlaceIndexes.Clear();
-            
-            for(int i = 0; i < field.Length; i++) {
+
+            for (int i = 0; i < field.Length; i++) {
                 if (field[i] == '#') {
                     _hashTagIndexes.Add(i);
                     _possiblePlaceIndexes.Add(i);
