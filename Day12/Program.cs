c0ad18380a533bd14357be1d6e7fc011a7090f5b
Robert Schneider <robert-schneider@live.de>
2023-12-15 15:45:05 +0100

    Day12 part2 cache implemented - some fields are not working

--- Day12/Program.cs
+++ Day12/Program.cs
@@ -5,7 +5,7 @@ namespace Day12 {
     internal class Program {
         private static List<int> _hashTagIndexes = [];
         private static List<int> _possiblePlaceIndexes = [];
-        private static int _amountOfIndexesToPlace = 0;
+        private static Dictionary<(int lengthIndex, int startIndex), long> combinationCache = [];
 
         static void Main(string[] args) {
             if (!ArgsValidator.IsValidArgs(args)) return;
@@ -16,67 +16,77 @@ namespace Day12 {
             long p2_score = 0;
 
             foreach (string line in File.ReadLines(args[0])) {
+                //Part1
                 string field = line.Split(' ')[0];
                 BuildFieldCaches(field);
                 int[] lenghts = line.Split(' ')[1].Split(',').Select(int.Parse).ToArray();
-                _amountOfIndexesToPlace = lenghts.Sum();
-                p1_score += TryCombinations(field, lenghts, [], 0);
+                p1_score += TryCombinations(field, lenghts, [], 0, _possiblePlaceIndexes.First());
+                combinationCache.Clear();
 
+                //Part2
                 fieldWatch.Start();
+
                 string unfoldField = $"{field}?{field}?{field}?{field}?{field}";
                 BuildFieldCaches(unfoldField);
+
                 int[] unfoldLengths = new int[lenghts.Length * 5];
                 lenghts.CopyTo(unfoldLengths, 0);
                 lenghts.CopyTo(unfoldLengths, lenghts.Length);
                 lenghts.CopyTo(unfoldLengths, lenghts.Length * 2);
                 lenghts.CopyTo(unfoldLengths, lenghts.Length * 3);
                 lenghts.CopyTo(unfoldLengths, lenghts.Length * 4);
-                _amountOfIndexesToPlace = unfoldLengths.Sum();
-                long possibilities = TryCombinations(unfoldField, unfoldLengths, [], 0);
-                Console.WriteLine($"Found after {fieldWatch.Elapsed} {possibilities} \tpossible solutions in field {field}.");
-                fieldWatch.Reset();
+
+                long possibilities = TryCombinations(unfoldField, unfoldLengths, [], 0, _possiblePlaceIndexes.First());
+                combinationCache.Clear();
+
+                Console.WriteLine($"Found after {fieldWatch.Elapsed} {possibilities} \tpossible solutions in field {field}");
                 p2_score += possibilities;
+
+                fieldWatch.Reset();
             }
 
             Console.WriteLine($"Part1 Result: {p1_score} ({p1_score == 21})\nPart2 Result: {p2_score} ({p2_score == 525152})");
             Console.WriteLine($"Elapsed: {stopwatch.Elapsed}");
         }
 
-        private static long TryCombinations(string field, int[] lenghts, List<int> indexList, int indexToSearch) {
-            if (_amountOfIndexesToPlace == indexList.Count) {
-                return CheckHashTags(indexList, partial: false) ? 1 : 0;
-            } else if (indexList.Count > 0 && !CheckHashTags(indexList, partial: true)) {
+        private static long TryCombinations(string field, int[] lengths, List<int> indexList, int indexToSearch, int startFieldIndex) {
+            //If indexToSearch is out of bounds, the max depths is reached
+            if (indexToSearch >= lengths.Length) {
+                return CheckCombination(indexList, partial: false) ? 1 : 0;
+            }
+
+            //Check, if this combination was searched for before
+            if (combinationCache.TryGetValue((indexToSearch, startFieldIndex), out long combinations)) {
+                return combinations;
+            }
+
+            //Check is partial combination is valid
+            if (indexList.Count > 0 && !CheckCombination(indexList, partial: true)) {
+                combinationCache.Add((indexToSearch, startFieldIndex), 0);
                 return 0;
             }
 
             long possibilities = 0;
-            int startIndex = indexToSearch == 0 ? _possiblePlaceIndexes.First() : indexList.Last() + 2;
-
-            for (int index = startIndex; index <= field.Length - lenghts[indexToSearch]; index++) {
-                bool impossible = false;
-                for (int checkIndex = index; checkIndex < index + lenghts[indexToSearch]; checkIndex++) {
-                    if (!_possiblePlaceIndexes.Contains(checkIndex)) {
-                        impossible = true;
-                        break;
-                    }
-                }
-                if (impossible) {
-                    continue;
+            for (int index = startFieldIndex; index <= field.Length - lengths[indexToSearch]; index++) {
+                List<int> nextIndexList = [.. indexList];
+                for (int length = 0; length < lengths[indexToSearch]; length++) {
+                    nextIndexList.Add(index + length);
                 }
 
-                //Build lengthIndexes
-                List<int> nextLengthIndexes = [.. indexList];
-                for (int length = 0; length < lenghts[indexToSearch]; length++) {
-                    nextLengthIndexes.Add(index + length);
-                }
-
-                possibilities += TryCombinations(field, lenghts, nextLengthIndexes, indexToSearch + 1);
+                possibilities += TryCombinations(field, lengths, nextIndexList, indexToSearch + 1, nextIndexList.Last() + 2);
             }
+            combinationCache.Add((indexToSearch, startFieldIndex), possibilities);
 
             return possibilities;
         }
 
-        private static bool CheckHashTags(List<int> indexList, bool partial) {
+        private static bool CheckCombination(List<int> indexList, bool partial) {
+            foreach(int index in indexList) {
+                if (!_possiblePlaceIndexes.Contains(index)) {
+                    return false;
+                }
+            }
+
             foreach (int hashTagIndex in _hashTagIndexes) {
                 if (partial && hashTagIndex > indexList.Last()) {
                     continue;
