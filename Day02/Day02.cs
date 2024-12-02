b2b57392d2c20dd2142f30ad3c9dff6524e0e651
Robert Schneider <robert-schneider@live.de>
2024-12-02 13:51:01 +0100

    Day02 Part1

--- Day02/Day02.cs
+++ Day02/Day02.cs
@@ -12,7 +12,26 @@ namespace Day02 {
             int p1_score = 0;
             int p2_score = 0;
 
-            //Do stuff
+            foreach(string line in File.ReadLines(args[0])) {
+                int[] numbers = line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+                bool isSafeLine = true;
+                bool ascending = numbers[0] < numbers[1];
+                int lastNumber = numbers[0];
+
+                for(int i = 1; i < numbers.Length; i++) {
+                    int difference = numbers[i] - numbers[i - 1];
+                    if(ascending) {
+                        isSafeLine &= difference >= 1 && difference <= 3;
+                    } else {
+                        isSafeLine &= difference <= -1 && difference >= -3;
+                    }
+                }
+
+                if(isSafeLine) {
+                    p1_score++;
+                }
+            }
 
             stopwatch.Stop();
             Console.WriteLine($"Part1 Result: {p1_score}\nPart2 Result: {p2_score}\nFinished in {stopwatch.Elapsed}");
