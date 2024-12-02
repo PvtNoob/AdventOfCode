be39dfb8e441bef7e3fe1282af487e4f8859e680
Robert Schneider <robert-schneider@live.de>
2024-12-02 12:57:22 +0100

    Stopwatches

--- Day02/Day02.cs
+++ Day02/Day02.cs
@@ -1,12 +1,21 @@
-﻿namespace Day02 {
+﻿using System.Diagnostics;
+using Shared;
+
+namespace Day02 {
     internal class Day02 {
         static void Main(string[] args) {
+            if(!ArgsValidator.IsValidArgs(args)) return;
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
             int p1_score = 0;
             int p2_score = 0;
 
-            //Do Code
+            //Do stuff
 
-            Console.WriteLine($"Part1 Result: {p1_score}\nPart2 Result: {p2_score}");
+            stopwatch.Stop();
+            Console.WriteLine($"Part1 Result: {p1_score}\nPart2 Result: {p2_score}\nFinished in {stopwatch.Elapsed}");
         }
     }
 }
