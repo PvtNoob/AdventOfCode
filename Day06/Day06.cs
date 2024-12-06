74cad642fd93eb917faccb2412c155a57c39299f
Robert Schneider <robert-schneider@live.de>
2024-12-06 10:59:15 +0100

    Day06 Part2

--- Day06/Day06.cs
+++ Day06/Day06.cs
@@ -16,20 +16,22 @@ namespace Day06 {
             Direction direction = Direction.Up;
             int maxRow = lines.Length - 1;
             int maxCol = lines[0].Length - 1;
-            List<(int, int)> visited = [];
+            List<(int row, int col, Direction dir)> visited = [];
+            (int row, int col) startPosition = (-1, -1);
             (int row, int col) position = (-1, -1);
 
             //Find Start
             for(int row = 0; row <= maxRow && position == (-1, -1); row++) {
                 for(int col = 0; col <= maxCol && position == (-1, -1); col++) {
                     if(lines[row][col] == '^') {
-                        position = (row, col);
-                        visited.Add((row, col));
+                        startPosition = (row, col);
+                        position = startPosition;
+                        visited.Add((row, col, direction));
                     }
                 }
             }
 
-            //Run
+            //Run Part1
             do {
                 (int row, int col) newPosition = position.Move(direction);
                 char charAtNewPosition = lines[newPosition.row][newPosition.col];
@@ -37,12 +39,38 @@ namespace Day06 {
                 if(charAtNewPosition == '#') {
                     direction = TurnRight(direction);
                 } else {
-                    visited.Add(newPosition);
+                    visited.Add((newPosition.row, newPosition.col, direction));
                     position = newPosition;
                 }
             } while(!position.Move(direction).IsOutOfBounds(maxRow, maxCol));
 
-            p1_score = visited.Distinct().Count();
+            IEnumerable<(int row, int col)> distinctVisitedFields = visited.Select(x => (x.row, x.col)).Distinct();
+            p1_score = distinctVisitedFields.Count();
+
+            foreach((int row, int col) field in distinctVisitedFields) {
+                //Reset Run
+                position = startPosition;
+                visited = [];
+                direction = Direction.Up;
+
+                //Do Run
+                do {
+                    (int row, int col) newPosition = position.Move(direction);
+                    char charAtNewPosition = lines[newPosition.row][newPosition.col];
+
+                    if(charAtNewPosition == '#' || newPosition == field) {
+                        direction = TurnRight(direction);
+                    } else {
+                        if(visited.Contains((newPosition.row, newPosition.col, direction))) {
+                            p2_score++;
+                            break;
+                        }
+
+                        visited.Add((newPosition.row, newPosition.col, direction));
+                        position = newPosition;
+                    }
+                } while(!position.Move(direction).IsOutOfBounds(maxRow, maxCol));
+            }
 
             stopwatch.Stop();
             Console.WriteLine($"Part1 Result: {p1_score}\nPart2 Result: {p2_score}\nFinished in {stopwatch.Elapsed}");
