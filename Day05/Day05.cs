8806ba75967e3da31e2ba4f2f2fe697299a3e750
Robert Schneider <robert-schneider@live.de>
2025-05-17 15:27:30 +0200

    Day 05

--- Day05/Day05.cs
+++ Day05/Day05.cs
@@ -1,8 +1,11 @@
-﻿using System.Diagnostics;
+﻿using System.Data;
+using System.Diagnostics;
 using Shared;
 
 namespace Day05 {
     internal class Day05 {
+        private static List<Rule> _rules = [];
+
         static void Main(string[] args) {
             if(!ArgsValidator.IsValidArgs(args)) return;
 
@@ -12,10 +15,61 @@ namespace Day05 {
             int p1_score = 0;
             int p2_score = 0;
 
-            //Do stuff
+            bool rulesFinished = false;
+            foreach(string line in File.ReadLines(args[0])) {
+                if(line.Length == 0) {
+                    rulesFinished = true;
+                    continue;
+                }
+
+                int[] pages = line.Split('|', ',').Select(int.Parse).ToArray();
+
+                if(!rulesFinished) {
+                    _rules.Add(new Rule(pages[0], pages[1]));
+                } else if(pages.Length > 0) {
+                    if(IsUpdateValid(pages)) {
+                        p1_score += pages.GetMiddleValue();
+                    } else {
+                        p2_score += FixUpdate(pages).GetMiddleValue();
+                    }
+                }
+            }
 
             stopwatch.Stop();
             Console.WriteLine($"Part1 Result: {p1_score}\nPart2 Result: {p2_score}\nFinished in {stopwatch.Elapsed}");
         }
+
+        private static bool IsUpdateValid(int[] update) {
+            for(int i = 0; i < update.Length; i++) {
+                foreach(Rule rule in _rules.Where(x => x.Before == update[i])) {
+                    int pageIndex = update.GetIndexOf(rule.After);
+
+                    if(pageIndex >= 0 && pageIndex < i) {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        private static int[] FixUpdate(int[] update) {
+            List<int> brokenUpdate = new(update);
+            List<int> fixedUpdate = new();
+            List<Rule> relevantRules = _rules.Where(rule => update.Contains(rule.Before) && update.Contains(rule.After)).ToList();
+
+            do {
+                int pageNotInAfterRules = brokenUpdate.First(page => !relevantRules.Any(rule => rule.After == page));
+
+                brokenUpdate.Remove(pageNotInAfterRules);
+                fixedUpdate.Add(pageNotInAfterRules);
+
+                relevantRules.RemoveAll(rule => rule.Before == pageNotInAfterRules);
+            } while(brokenUpdate.Count > 0);
+
+            return fixedUpdate.ToArray();
+        }
     }
+
+    internal record Rule(int Before, int After);
 }
--- Shared/NumberExtensions.cs
+++ Shared/NumberExtensions.cs
@@ -3,5 +3,33 @@
         public static bool IsBetween(this long number, long min, long max) {
             return number >= min && number <= max;
         }
+
+        public static int GetMiddleValue(this int[] values) {
+            if(values.Length == 0) return -1;
+            return values[(int)Math.Floor(new decimal(values.Length) / 2)];
+        }
+
+        public static int GetIndexOf(this int[] values, int value) {
+            for(int i = 0; i < values.Length; i++) {
+                if(values[i] == value) {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public static int[] Shuffle(this int[] values) {
+            Random random = new();
+            for(int i = 0; i < values.Length; i++) {
+                int randomIndex = random.Next(0, values.Length);
+                (values[i], values[randomIndex]) = (values[randomIndex], values[i]);
+            }
+            return values;
+        }
+
+        public static string GetString(this List<int> values) {
+            return string.Join(", ", values);
+        }
     }
 }
